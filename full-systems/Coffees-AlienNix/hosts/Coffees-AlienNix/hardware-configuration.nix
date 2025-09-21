4db47fc10c164fc10710c4ed87495cff8e7c09f0
Coffeecan5577 <c.curry2210@gmail.com>
2025-09-21 02:45:09 -0500

    Added mount point for external hard drive

--- full-systems/Coffees-AlienNix/hosts/Coffees-AlienNix/hardware-configuration.nix
+++ full-systems/Coffees-AlienNix/hosts/Coffees-AlienNix/hardware-configuration.nix
@@ -29,6 +29,11 @@
       fsType = "btrfs";
     };
 
+  fileSystems."/media/External-Drive" = 
+    { device = "/dev/disk/by-uuid/0ddfc82d-e2c8-4b98-b907-fa09bceedfd7";
+      fsType = "btrfs"; 
+    };
+
   swapDevices =
     [ { device = "/dev/disk/by-uuid/d0ee95ac-fa4d-4b96-a584-0dc2fbb5a689"; }
     ];
