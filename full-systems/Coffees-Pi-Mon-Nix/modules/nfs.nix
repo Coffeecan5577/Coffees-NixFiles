6df46409b11270326f879f1e9d938a380ea714cd
Coffeecan5577 <c.curry2210@gmail.com>
2025-10-03 04:03:35 -0500

    Modified NFS share file paths on Pi-Mon-Nix

--- full-systems/Coffees-Pi-Mon-Nix/modules/nfs.nix
+++ full-systems/Coffees-Pi-Mon-Nix/modules/nfs.nix
@@ -1,7 +1,7 @@
 {
   services.nfs.server.enable = true;
   services.nfs.server.exports = ''
-    /media/Homelab-Drive			10.10.1.123(rw,fsid=0,no_subtree_check)
-    /media/Homelab-Drive/Coffees-Homelab	10.10.1.123(rw,nohide,insecure,no_subtree_check)
+    ~/media/Homelab-Drive			10.10.1.123(rw,fsid=0,no_subtree_check,sync)
+    ~/media/Homelab-Drive/Coffees-Homelab	10.10.1.123(rw,nohide,insecure,no_subtree_check,sync)
   '';
 }
