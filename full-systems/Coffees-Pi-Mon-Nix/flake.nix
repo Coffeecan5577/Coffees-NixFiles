50ec8aae126c2a9bc80787f1aa4da739424e487c
Coffeecan5577 <c.curry2210@gmail.com>
2025-11-27 15:32:02 -0600

    Updated Pi-Mon-Nix flake version to 25.11

--- full-systems/Coffees-Pi-Mon-Nix/flake.nix
+++ full-systems/Coffees-Pi-Mon-Nix/flake.nix
@@ -4,25 +4,25 @@
   inputs = {
     
     # Nix Packages URL
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
  
     # Custom Japanese Font import URL
-    nixos-fonts.url = "github:Takamatsu-Naoki/nixos-fonts";
+    # nixos-fonts.url = "github:Takamatsu-Naoki/nixos-fonts";
 
     home-manager = {
-      url = "github:nix-community/home-manager/release-25.05";
+      url = "github:nix-community/home-manager/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    stylix = {
-      url = "github:nix-community/stylix/release-25.05";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };   
+    # stylix = {
+      # url = "github:nix-community/stylix/release-25.05";
+      # inputs.nixpkgs.follows = "nixpkgs";
+    # };   
   };
 
   outputs = { self, nixpkgs, home-manager, ... }@inputs: let
     system = "aarch64-linux";
-    homeStateVersion = "25.05";
+    homeStateVersion = "25.11";
     user = "coffeecan";
     hosts = [
       { hostname = "Coffees-Pi-Mon-Nix"; stateVersion = "25.05"; }
