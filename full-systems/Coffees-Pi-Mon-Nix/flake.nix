f564de69dd4a76ce72df33cfa6645ac329b07863
Coffeecan5577 <c.curry2210@gmail.com>
2025-12-01 04:29:17 -0600

    Updated Pi-Mon-Nix host version to 25.11 in flake file

--- full-systems/Coffees-Pi-Mon-Nix/flake.nix
+++ full-systems/Coffees-Pi-Mon-Nix/flake.nix
@@ -25,7 +25,7 @@
     homeStateVersion = "25.11";
     user = "coffeecan";
     hosts = [
-      { hostname = "Coffees-Pi-Mon-Nix"; stateVersion = "25.05"; }
+      { hostname = "Coffees-Pi-Mon-Nix"; stateVersion = "25.11"; }
     ];
 
     makeSystem = { hostname, stateVersion }: nixpkgs.lib.nixosSystem {
