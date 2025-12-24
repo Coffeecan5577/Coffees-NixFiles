c82e1a03e8b95648e391c853c581fdc0cc9acca7
Coffeecan5577 <c.curry2210@gmail.com>
2025-12-24 05:45:59 -0600

    Experimenting with file systems and mount points in VM1 Nix VM

--- full-systems/Coffees-NixVM1/home-manager/modules/waybar/default.nix
+++ full-systems/Coffees-NixVM1/home-manager/modules/waybar/default.nix
@@ -108,7 +108,7 @@
           format = "󱁋 Disk: {} used";
           interval = 300;
           max_length = 20;
-          exec = "df --total -hl | grep '/' | awk '{print $5}'";
+          exec = "df --total -hl | grep '/dev/vda3' | awk '{print $5}'";
         };
       };
     };
