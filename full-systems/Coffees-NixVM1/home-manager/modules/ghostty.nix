d2ed243dac3bca713cafd38730e92555b7c0dec6
Coffeecan5577 <c.curry2210@gmail.com>
2025-12-25 09:13:05 -0600

    Added = to background opacity assignment to VM1 ghostty.nix

--- full-systems/Coffees-NixVM1/home-manager/modules/ghostty.nix
+++ full-systems/Coffees-NixVM1/home-manager/modules/ghostty.nix
@@ -9,7 +9,7 @@
         font-size = 16;
 
         # Set windows transparency value and blur background
-        background-opacity - 0.8;
+        background-opacity = 0.8;
   
         # Background blur value can be set to a non-negative integer, true or false
         background-blur = 5;
