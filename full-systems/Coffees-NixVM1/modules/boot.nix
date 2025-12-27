8392226fde9a872ff050ddade68d1805596531cd
Coffeecan5577 <c.curry2210@gmail.com>
2025-12-27 10:08:38 -0600

    Enabled plymouth boot sequence with catppuccin

--- full-systems/Coffees-NixVM1/modules/boot.nix
+++ full-systems/Coffees-NixVM1/modules/boot.nix
@@ -1,4 +1,11 @@
+{ pkgs, ...}:
 {
   boot.loader.grub.enable = true;
   boot.loader.grub.device = "/dev/vda";
+  boot.plymouth = {
+    enable = true;
+    font = "${pkgs.jetbrains-mono}/share/fonts/truetype/JetBrainsMono-Regular.ttf";
+    themePackages = [ pkgs.catppuccin-plymouth ];
+    theme = "catppuccin-macchiato";
+  };
 }
