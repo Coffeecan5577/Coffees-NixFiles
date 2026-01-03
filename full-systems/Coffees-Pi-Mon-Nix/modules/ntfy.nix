6e15ad7721084b266daf9adb41f3bd845aafd432
Coffeecan5577 <c.curry2210@gmail.com>
2026-01-02 18:41:12 -0600

    Disabled password authentication for Pi-Mon-Nix

--- full-systems/Coffees-Pi-Mon-Nix/modules/ntfy.nix
+++ full-systems/Coffees-Pi-Mon-Nix/modules/ntfy.nix
@@ -3,8 +3,6 @@
 {
   services.ntfy-sh = {
     enable = true;
-    # group = "coffeecan";
-    # user = "coffeecan";
     settings = {
      listen-http = ":8005";
      base-url = "http://ntfy.coffees-firelink";
