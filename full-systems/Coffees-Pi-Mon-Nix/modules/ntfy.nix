d8da60a7cb896c449c313e55cb1dc42d650f03f9
Coffeecan5577 <c.curry2210@gmail.com>
2026-01-02 18:39:47 -0600

    Commented out user and group lines in ntfy.nix

--- full-systems/Coffees-Pi-Mon-Nix/modules/ntfy.nix
+++ full-systems/Coffees-Pi-Mon-Nix/modules/ntfy.nix
@@ -3,8 +3,8 @@
 {
   services.ntfy-sh = {
     enable = true;
-    group = "coffeecan";
-    user = "coffeecan";
+    # group = "coffeecan";
+    # user = "coffeecan";
     settings = {
      listen-http = ":8005";
      base-url = "http://ntfy.coffees-firelink";
