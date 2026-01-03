7c2c4292fc87a544e2bdc6170ffcd0fd6bc9d495
Coffeecan5577 <c.curry2210@gmail.com>
2026-01-03 05:38:13 -0600

    Changed hard drive from vda to sda for boot.nix

--- full-systems/Coffees-NixVM1/modules/boot.nix
+++ full-systems/Coffees-NixVM1/modules/boot.nix
@@ -1,7 +1,7 @@
 { pkgs, ...}:
 {
   boot.loader.grub.enable = true;
-  boot.loader.grub.device = "/dev/vda";
+  boot.loader.grub.device = "/dev/sda";
   boot.plymouth = {
     enable = true;
     font = "${pkgs.jetbrains-mono}/share/fonts/truetype/JetBrainsMono-Regular.ttf";
