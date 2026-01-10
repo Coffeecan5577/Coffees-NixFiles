90fdb63ae0fe68812ef5e3e6c1eff10695cec386
Coffeecan5577 <c.curry2210@gmail.com>
2026-01-10 09:38:49 -0600

    Finished base configuration of Zed-Editor package in home-manager

--- full-systems/Coffees-NixOS/home-manager/modules/zed-editor.nix
+++ full-systems/Coffees-NixOS/home-manager/modules/zed-editor.nix
@@ -11,5 +11,27 @@
       "python-snippets"
       "tmux"
     ];
+    userSettings = {
+      buffer_font_family = "Segoe UI";
+      icon_theme = "Colored Zed Icons Theme Dark";
+      base_keymap = "VSCode";
+      telemetry = { 
+        diagnostics = true;
+        metrics = true;
+      };
+      ui_font_family = "Liberation Sans";
+      ui_font_size = 20;
+      buffer_font_size = 18;
+      theme = {
+        mode = "system";
+        light = "Gruvbox Dark";
+        dark = "Gruvbox Dark";
+      };
+      terminal = {
+        font_family = "Monaspace Neon";
+        font_size = 18;
+        line_height = "standard";
+      };
+    };
   };
 }
