bb2ee75f2b13fb32951116e47289b1e24fad66b2
Coffeecan5577 <c.curry2210@gmail.com>
2026-02-16 09:11:04 -0600

    Rewrote some of nvf.nix in modules

--- full-systems/Coffees-NixOS/modules/nvf.nix
+++ full-systems/Coffees-NixOS/modules/nvf.nix
@@ -6,19 +6,27 @@
     # Settings need to go under the settings attribute set
     settings = {
       vim = {
+       
+        # Theme Configuration settings
         theme = {
           enable = true;
           name = "gruvbox";
           style = "dark";
         };
+
+        # Enabling LSP support for programming languages
+        lsp = {
+          enable = true;
+        };
+
+        # Enabling plugins
         statusline.lualine.enable = true;
         telescope.enable = true;
         autocomplete.nvim-cmp.enable = true;
 
+        # Enabling language support
         languages = {
-          enableLSP = true;
           enableTreesitter = true;
-
           nix.enable = true;
           go.enable = true;
           python.enable = true;
