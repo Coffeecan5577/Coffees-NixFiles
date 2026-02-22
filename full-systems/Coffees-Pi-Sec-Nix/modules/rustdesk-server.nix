0cccc8c38af8a93234e19c9f6d15f491dfe53d25
Coffeecan5577 <c.curry2210@gmail.com>
2026-02-22 09:57:05 -0600

    Completed setup of Rustdesk server on Pi-Sec-Nix

--- full-systems/Coffees-Pi-Sec-Nix/modules/rustdesk-server.nix
+++ full-systems/Coffees-Pi-Sec-Nix/modules/rustdesk-server.nix
@@ -2,5 +2,12 @@
   services.rustdesk-server = {
      enable = true;
      openFirewall = true;
+     signal = {
+       enable = true;
+       relayHosts = ["10.10.1.124"];
+     };
+     relay = {
+       enable = true;
+     };
    };
 }
