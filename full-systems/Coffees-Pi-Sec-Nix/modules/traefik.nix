c9625eb58fac6dccb93f9002d721a085a97ca10a
Coffeecan5577 <c.curry2210@gmail.com>
2026-03-22 16:36:09 -0500

    Configured basic setup for declarative traefik service on Pi-Sec-Nix

---
+++ full-systems/Coffees-Pi-Sec-Nix/modules/traefik.nix
@@ -0,0 +1,25 @@
+{pkgs, config, ...}:
+{
+  services.traefik = {
+    enable = true;
+
+   staticConfigOptions = {
+      entryPoints = {
+        web = {
+          address = ":80";
+          asDefault = true;
+          http.redirections.entrypoint = {
+            to = "websecure";
+            scheme = "https";
+          };
+        };
+       
+        websecure = {
+          address = ":443";
+          asDefault = true;
+          http.tls.certresolver = "letsencrypt";
+        };
+      };
+    };
+  };
+}
