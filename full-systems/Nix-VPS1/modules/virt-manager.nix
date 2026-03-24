244d019a4d30b01266ac9b3bde129023091a5fe1
Coffeecan5577 <c.curry2210@gmail.com>
2026-03-24 10:27:09 -0500

    Removed unnecessary modules from Nix-VPS1

--- full-systems/Nix-VPS1/modules/virt-manager.nix
+++
@@ -1,9 +0,0 @@
-{ pkgs, ... }:
-{
-  programs.virt-manager = {
-     enable = true;
-  };
-  users.groups.libvirtd.members = ["coffeecan"];
-  virtualisation.libvirtd.enable = true;
-  virtualisation.spiceUSBRedirection.enable = true;
-}
