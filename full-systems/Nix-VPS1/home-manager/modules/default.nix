4cf47b29bacf31b99ebf61ad650200f097ddf875
Coffeecan5577 <c.curry2210@gmail.com>
2026-03-24 10:39:44 -0500

    Removed unnecessary home manager module files and directories

--- full-systems/Nix-VPS1/home-manager/modules/default.nix
+++ full-systems/Nix-VPS1/home-manager/modules/default.nix
@@ -1,25 +1,13 @@
 {
   imports = [
     ./bat.nix
-    ./btop-rocm.nix
     ./cursor.nix
-    ./dconf.nix
     ./eza.nix
     ./fastfetch.nix
     ./fish.nix
     ./fonts.nix
     ./ghostty.nix
     ./git.nix
-    ./hyprland
-    ./keepassxc.nix
-    ./librewolf.nix
     ./oh-my-posh.nix
-    ./ranger.nix
-    ./swaync
-    ./tmux.nix
-    ./waybar
-    ./wofi
-    ./zathura.nix
-    ./zed-editor.nix
   ];
 }
--- full-systems/Nix-VPS1/home-manager/modules/hyprland/hyprpaper.nix
+++
@@ -1,9 +0,0 @@
-{
-  services.hyprpaper = {
-    enable = true;
-    settings = {
-      preload = [ "~/Pictures/Wallpapers/Nix_Rainbow.png" ];
-      wallpaper = [ ",~/Pictures/Wallpapers/Nix_Rainbow.png" ];  
-    };
-  };
-}
