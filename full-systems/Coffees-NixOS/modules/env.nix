976fd8e4404ada29c7d554199e17c04d8013fa5e
Coffeecan5577 <c.curry2210@gmail.com>
2026-04-01 04:30:39 -0500

    Changed primary editor from nano to nvim in env.nix

--- full-systems/Coffees-NixOS/modules/env.nix
+++ full-systems/Coffees-NixOS/modules/env.nix
@@ -1,7 +1,7 @@
 {
   environment.sessionVariables = rec {
     TERMINAL = "ghostty";
-    EDITOR = "nano";
+    EDITOR = "nvim";
     SOPS_AGE_KEY_FILE = "$HOME/.config/sops/age/Pi-Sec-Age-Key.txt";
     # NIXPKGS_ALLOW_INSECURE = 1;
     };
