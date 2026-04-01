35547035cb34df7c0d872ff82eaed42db8cece1c
Coffeecan5577 <c.curry2210@gmail.com>
2026-03-31 20:42:58 -0500

    Commenting out homeModule imports for niri and noctalia-shell

--- full-systems/Nix-VPS1/home-manager/modules/niri/default.nix
+++ full-systems/Nix-VPS1/home-manager/modules/niri/default.nix
@@ -2,7 +2,7 @@
 {
   imports = [
     # ./binds.nix
-    inputs.niri.homeModules.niri # Import niri's home-manager module
+    # inputs.niri.homeModules.niri # Import niri's home-manager module
     ./main.nix
     ./noctalia-shell.nix
     ];
