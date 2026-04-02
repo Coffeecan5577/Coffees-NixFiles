da1d0a30f9d051860622272e842fead1f96cb082
Coffeecan5577 <c.curry2210@gmail.com>
2026-04-02 05:22:01 -0500

    Adjusted home-manager files for niri and noctalia shell

--- full-systems/Nix-VPS1/home-manager/modules/niri/default.nix
+++ full-systems/Nix-VPS1/home-manager/modules/niri/default.nix
@@ -1,8 +1,6 @@
-{ inputs, ...}:
 {
   imports = [
     # ./binds.nix
-    inputs.niri-flake.homeModules.niri # Import niri's home-manager module
     ./main.nix
     ./noctalia-shell.nix
     ];
--- full-systems/Nix-VPS1/home-manager/modules/niri/main.nix
+++ full-systems/Nix-VPS1/home-manager/modules/niri/main.nix
@@ -1,7 +1,12 @@
-{ pkgs, ...}:
 {
-  programs.niri = {
-    enable = true;
-    package = pkgs.niri;
+programs.niri = {
+  enable = true;
+  settings = {
+    prefer-no-csd = true;
+    input.keyboard.xkb = {
+      layout = "us";
+      variant = "intl";
+      };
+    };
   };
 }
