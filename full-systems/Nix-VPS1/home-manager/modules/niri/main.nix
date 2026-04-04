a459b13ace5e5200182defec523de5f43d37a510
Coffeecan5577 <c.curry2210@gmail.com>
2026-04-03 19:15:01 -0500

    Configured niri using home-manager module settings

--- full-systems/Nix-VPS1/home-manager/modules/niri/main.nix
+++ full-systems/Nix-VPS1/home-manager/modules/niri/main.nix
@@ -1,18 +1,60 @@
 {	
-  programs.niri = {
-     settings = {
-       prefer-no-csd = true;
-        input.keyboard.xkb = {
-          layout = "us";
-          variant = "intl";
-        };
-    layout = {
-      gaps = 4;
-      focus-ring = {
-        enable = true;
-        width = 2;
-        };
+  wayland.windowManager.niri = {
+    enable = true;
+    settings = {
+      output = [
+        {
+          _args = ["DP-0"];
+          mode = "1920x1080@60";
+          position._props = 
+            {
+              x = 0;
+              y = 0;
+            };
+        }
+      ];
+
+     input = {
+       keyboard.xkb = {
+       layout = "us";
+       };
+  };
+  layout = {
+    preset-column-widths._children = [
+      {proportion = 0.33333;}
+      {proportion = 0.5;}
+      {proportion = 0.66667;}
+      {proportion = 1.0;}
+    ];
+    focus-ring.off = [];
+     };
+  
+     window-rule = [
+    {
+      block-out-from = "screen-capture";
+    }
+  ];
+
+  binds = {
+    "Mod+TouchpadScrollDown" = {
+      _props = {
+        cooldown-ms = 500;
       };
+      focus-workspace-down = [];
+    };
+    "Mod+T" = {
+      spawn = "ghostty";
+    };
+    XF86AudioRaiseVolume = {
+      spawn-sh = [
+        "wpctl" "set-volume" "@DEFAULT_AUDIO_SINK@" "0.1+"
+      ];
     };
   };
+  spawn-sh-at-startup = [
+    ["wl-paste --type text --watch cliphist store"]
+    ["wl-paste --type image --watch cliphist store"]
+  ];
+  };
+ };
 }
