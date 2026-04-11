90551ca9b49ebdbb21ff609ba92360d79e832370
Coffeecan5577 <c.curry2210@gmail.com>
2026-04-11 07:41:17 -0500

    Modified package sources for ranger plugins in ranger.nix

--- full-systems/Coffees-NixOS/home-manager/modules/ranger.nix
+++ full-systems/Coffees-NixOS/home-manager/modules/ranger.nix
@@ -1,3 +1,4 @@
+{ pkgs, ... }:
 {
   programs.ranger = {
     enable = true;
@@ -23,23 +24,29 @@
     plugins = [
       {
         name = "ranger-archives";
-        src = builtins.fetchGit {
-          url = "https://github.com/maximtrp/ranger-archives";
-          ref = "master";
+        src = pkgs.fetchFromGitHub {
+          owner = "maximtrp";
+          repo = "ranger-archives";
+          rev = "0b1cfa9a77412c3b51da5b1b213c672227f9fbb4";
+          hash = "sha256-HEJ+8KlG++PK0vVpEYptbyuPZAKllX5PeyaTBKcf+8M=";
         };
       }
       {
         name = "ranger-devicons2";
-        src = builtins.fetchGit {
-          url = "https://github.com/cdump/ranger-devicons2";
-          ref = "master";
+        src = pkgs.fetchFromGitHub {
+          owner = "cdump";
+          repo = "ranger-devicons2";
+          rev = "94bdcc19218681debb252475fd9d11cfd274d9b1";
+          hash = "sha256-aJCIoDfzmOnzMLlbOe+dy6129n5Dc4OrefhHnPsgI8k=";
         };
       }
       {
         name = "ranger_tmux";
-        src = builtins.fetchGit {
-          url = "https://github.com/joouha/ranger_tmux";
-          ref = "master";
+        src = pkgs.fetchFromGitHub {
+          owner = "joouha";
+          repo = "ranger_tmux";
+          rev = "05ba5ddf2ce5659a90aa0ada70eb1078470d972a";
+          hash = "sha256-KCBOPwhG4U/k2a/Dp/+fZeetFz/PW9424zi3NlLsDj0=";
         };
       }
     ];
