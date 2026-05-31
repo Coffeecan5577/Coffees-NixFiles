cd3137c31418a9b9e85ee992797a6d3e9f07203b
Coffeecan5577 <c.curry2210@gmail.com>
2026-05-31 16:07:08 -0500

    Adding drift.nix file for nix configuration of CLI screensaver

---
+++ full-systems/Coffees-NixOS/home-manager/modules/drift.nix
@@ -0,0 +1,27 @@
+{
+  drift.settings = {
+    engine = {
+      fps = 60;
+      cycle_seconds = 30;
+      fade_seconds = 0.3;
+      scenes = [
+        "rain"
+        "orrery"
+      ];
+      theme = "gruvbox";
+      shuffle = true;
+      hide_tmux_status = false;
+    };
+
+    scene.rain = {
+      charset = "ｱｲｳｴｵｶｷｸｹｺｻｼｽｾｿﾀﾁﾂﾃﾄﾅﾆﾇﾈﾉ0123456789";
+      density = 0.4;
+      speed = 1.0;
+    };
+
+    scene.orrery = {
+      bodies = 8;
+      trail_decay = 2.4;
+    };
+  };
+}
