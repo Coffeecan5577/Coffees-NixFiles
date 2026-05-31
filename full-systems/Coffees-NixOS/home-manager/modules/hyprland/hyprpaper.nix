5842f65fdc2a238744b00e2320167e14754f4f24
Coffeecan5577 <c.curry2210@gmail.com>
2026-05-31 10:01:45 -0500

    Refactored hyprpaper.nix syntax

--- full-systems/Coffees-NixOS/home-manager/modules/hyprland/hyprpaper.nix
+++ full-systems/Coffees-NixOS/home-manager/modules/hyprland/hyprpaper.nix
@@ -2,8 +2,20 @@
   services.hyprpaper = {
     enable = true;
     settings = {
-      preload = [ "~/Coffees-NixFiles/full-systems/Coffees-NixOS/home-manager/modules/hyprland/wallpapers/Nix_Gruvbox_Rainbow.png" ];
-      wallpaper = [ ",~/Coffees-NixFiles/full-systems/Coffees-NixOS/home-manager/modules/hyprland/wallpapers/Nix_Gruvbox_Rainbow.png" ];  
+      splash = false;
+      wallpaper = [
+        {
+          monitor = "eDP-1";
+          path = "~/Coffees-NixFiles/full-systems/Coffees-NixOS/home-manager/modules/hyprland/wallpapers/Nix_Gruvbox_Rainbow.png";
+          # fit_mode = "fill";
+        }
+ 
+        {
+          monitor = "HDMI-A-1";
+          path = "~/Coffees-NixFiles/full-systems/Coffees-NixOS/home-manager/modules/hyprland/wallpapers/Nix_Gruvbox_Rainbow.png";
+          # fit_mode = "fill";
+        }
+      ];   
     };
   };
 }
