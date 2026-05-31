5f18d801289e042cb84415b83b6dd91cf20ebb8f
Coffeecan5577 <c.curry2210@gmail.com>
2026-05-31 18:37:40 -0500

    Added zen-browser to local-packages.nix

--- full-systems/Coffees-NixOS/hosts/Coffees-NixOS/local-packages.nix
+++ full-systems/Coffees-NixOS/hosts/Coffees-NixOS/local-packages.nix
@@ -1,5 +1,6 @@
 { pkgs, inputs, ... }: {
   environment.systemPackages = with pkgs; [
     inputs.nixmate.packages.${stdenv.hostPlatform.system}.default
+    inputs.zen-browser.packages.${stdenv.hostPlatform.system}.twilight
   ];
 }
