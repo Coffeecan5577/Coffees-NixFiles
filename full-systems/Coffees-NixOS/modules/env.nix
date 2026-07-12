55ae51f72316ebf97e40090ec895108839e437f0
Coffeecan5577 <c.curry2210@gmail.com>
2026-07-12 13:39:23 -0500

    Experimenting with nvim manpager settings

--- full-systems/Coffees-NixOS/modules/env.nix
+++ full-systems/Coffees-NixOS/modules/env.nix
@@ -4,8 +4,8 @@
     EDITOR = "nvim";
     SOPS_AGE_KEY_FILE = "$HOME/.config/sops/age/Pi-Sec-Age-Key.txt";
     # NIXPKGS_ALLOW_INSECURE = 1;
-    MANPAGER = "nvim -c 'set ft=man' -";
-    PAGER = "nvim -c 'set ft=man' -";
+    MANPAGER = "nvim -c +Man!";
+    PAGER = "nvim -c +Man!";
     };
 }
 
