c361d5595d7b87f37e53af176dc3e7bffb9e4377
Coffeecan5577 <c.curry2210@gmail.com>
2026-07-15 04:40:36 -0500

    Cleaned up gamemode options

--- full-systems/Coffees-NixOS/modules/steam.nix
+++ full-systems/Coffees-NixOS/modules/steam.nix
@@ -8,9 +8,11 @@
  # Enabling gamemode option
  programs.gamemode = {
     enable = true;
-    custom = {
-      start = "${pkgs.libnotify}/bin/notify-send 'GameMode started'";
-      end = "${pkgs.libnotify}/bin/notify-send 'GameMode ended'";
+    settings = {
+      custom = {
+        start = "${pkgs.libnotify}/bin/notify-send 'GameMode started'";
+        end = "${pkgs.libnotify}/bin/notify-send 'GameMode ended'";
+      };
     };
   };
 }
