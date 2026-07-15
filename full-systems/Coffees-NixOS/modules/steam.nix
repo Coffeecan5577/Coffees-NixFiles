765c94a13b8b8d2611a7f72e0e466822485fcf33
Coffeecan5577 <c.curry2210@gmail.com>
2026-07-15 04:34:54 -0500

    Added custom gamemode settings in steam.nix

--- full-systems/Coffees-NixOS/modules/steam.nix
+++ full-systems/Coffees-NixOS/modules/steam.nix
@@ -1,9 +1,16 @@
+{pkgs, ...}:
+
 {
   programs.steam = {
     enable = true;
   };
 
  # Enabling gamemode option
- programs.gamemode.enable = true;
-
+ programs.gamemode = {
+    enable = true;
+    custom = {
+      start = "${pkgs.libnotify}/bin/notify-send 'GameMode started'";
+      end = "${pkgs.libnotify}/bin/notify-send 'GameMode ended'";
+    };
+  };
 }
