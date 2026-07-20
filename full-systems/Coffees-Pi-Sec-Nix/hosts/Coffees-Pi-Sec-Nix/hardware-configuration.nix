648c2d22d2c6bd879f305f1e86d16b1a9e11a93a
Coffeecan5577 <coffeecan@tutamail.com>
2026-07-20 18:27:32 -0500

    Changed dhcp service to enabled in dns.nix.

--- full-systems/Coffees-Pi-Sec-Nix/hosts/Coffees-Pi-Sec-Nix/hardware-configuration.nix
+++ full-systems/Coffees-Pi-Sec-Nix/hosts/Coffees-Pi-Sec-Nix/hardware-configuration.nix
@@ -22,7 +22,7 @@
   # still possible to use this option, but it's recommended to use it in conjunction
   # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
   networking.useDHCP = lib.mkDefault true;
-  networking.interfaces.end0.useDHCP = lib.mkDefault true;
+  # networking.interfaces.end0.useDHCP = lib.mkDefault true;
   # networking.interfaces.enp11s0.useDHCP = lib.mkDefault true;
   # networking.interfaces.enp12s0f3u2u2.useDHCP = lib.mkDefault true;
   # networking.interfaces.wlp9s0.useDHCP = lib.mkDefault true;
