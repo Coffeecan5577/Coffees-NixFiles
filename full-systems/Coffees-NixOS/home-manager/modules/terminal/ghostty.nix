736a93c1420345adf998477efd4decb8cbc0d485
Coffeecan5577 <c.curry2210@gmail.com>
2026-08-08 09:02:16 -0500

    Changed ghostty theme to Gruvbox Light

--- full-systems/Coffees-NixOS/home-manager/modules/terminal/ghostty.nix
+++ full-systems/Coffees-NixOS/home-manager/modules/terminal/ghostty.nix
@@ -5,11 +5,11 @@
     enableFishIntegration = true;
     settings = 
       {
-        theme = "Gruvbox Light Hard";
+        theme = "Gruvbox Light";
         font-size = 16;
         
         # Set windows transparency value and blur background
-        background-opacity = 0.8;
+        background-opacity = 0.9;
         
         # Background blur value can be set to a non-negative integer, true or false
         background-blur = 5;
