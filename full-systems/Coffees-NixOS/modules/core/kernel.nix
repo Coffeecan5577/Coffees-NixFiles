b8171d50b84b3e6dc94024e06fcf245a07e9aa67
Coffeecan5577 <c.curry2210@gmail.com>
2026-09-03 20:04:14 -0500

    Removed troubling kernel modules causing potential screen freezes

--- full-systems/Coffees-NixOS/modules/core/kernel.nix
+++ full-systems/Coffees-NixOS/modules/core/kernel.nix
@@ -15,11 +15,8 @@
       "systemd.show_status=auto"
       "fbcon=nodefer"
       "vt.global_cursor_default=0"
-      "kernel.modules_disabled=1"
-      "lsm=landlock,lockdown,yama,integrity,apparmor,bpf"
       "usbcore.autosuspend=-1"
       "video4linux"
-      "acpi_rev_override=5"
     ];
 
     # boot.kernelPatches = [ {
